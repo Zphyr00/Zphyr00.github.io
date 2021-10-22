4318e7520fec0cd6752f2d1bee661c73c7f6a06b
Zphyr00 <92588368+Zphyr00@users.noreply.github.com>
2021-10-22 16:32:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,12 +24,12 @@ function networkOrCache(request) {
 }
 
 const FALLBACK =
-    '<div>\n' +
-    '    <div>Z_phyr\'s personal page</div>\n' +
-    '    <div>Личная страница Z_phyr</div>\n' +
-    '    <div>Dude, you\'re offline</div>\n' +
-    '    <div>Чувак, ты в оффлайне</div>\n' +
-    '    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: rgb(241, 242, 243) none repeat scroll 0% 0%; display: block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"><g><path d="M50 8A42 42 0 1 0 92 50.00000000000001" fill="none" stroke="#6d04af" stroke-width="8"></path><path d="M49 -2L49 18L59 8L49 -2" fill="#6d04af"></path><animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="4s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform></g></svg>\n' +
+    '<div style="margin-left:auto;margin-right:auto;">\n' +
+    '    Z_phyr\'s personal page\n' +
+    '    Личная страница Z_phyr\n' +
+    '    Dude, you\'re offline\n' +
+    '    Чувак, ты в оффлайне\n' +
+    '    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none repeat scroll 0% 0%; display: block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"><g><path d="M50 8A42 42 0 1 0 92 50.00000000000001" fill="none" stroke="#6d04af" stroke-width="8"></path><path d="M49 -2L49 18L59 8L49 -2" fill="#6d04af"></path><animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="4s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform></g></svg>\n' +
     '</div>';
 
 function useFallback() {
