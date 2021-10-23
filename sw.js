8684092ef6029f1ff048f2f0e63d2709ba9c91b7
Zphyr00 <92588368+Zphyr00@users.noreply.github.com>
2021-10-23 15:12:22 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,42 @@
-const CACHE = 'offline-fallback';
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches
-            .open(CACHE)
-            .then(() => self.skipWaiting())
+const CACHE_NAME = "offline";
+const OFFLINE_URL = "https://zphyr00.github.io/offline.html";
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+    })()
+  );
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+  self.clients.claim();
+});
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          console.log("Fetch failed; returning offline page instead.", error);
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
     );
+  }
 });
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(networkOrCache(event.request)
-        .catch(() => useFallback()));
-});
-
-function networkOrCache(request) {
-    return fetch(request)
-        .then((response) => response.ok ? response : fromCache(request))
-        .catch(() => fromCache(request));
-}
-
-const FALLBACK =
-    '<div style="text-align:center;margin-left:auto;margin-right:auto">' +
-    '<p>Z_phyr\'s personal page</p>' +
-    '<p>Личная страница Z_phyr</p>' +
-    '<p>Dude, you\'re offline</p>' +
-    '<p>Чувак, ты в оффлайне</p>' +
-    '<p><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none repeat scroll 0% 0%; display: block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"><g><path d="M50 8A42 42 0 1 0 92 50.00000000000001" fill="none" stroke="#6d04af" stroke-width="8"></path><path d="M49 -2L49 18L59 8L49 -2" fill="#6d04af"></path><animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="4s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform></g></svg></p>' +
-    '</div>';
-
-function useFallback() {
-    return Promise.resolve(new Response(FALLBACK, { headers: {
-        'Content-Type': 'text/html; charset=utf-8'
-    }}));
-}
-
-function fromCache(request) {
-    return caches.open(CACHE).then((cache) =>
-        cache.match(request).then((matching) =>
-            matching || Promise.reject('no-match')
-        ));
-}
