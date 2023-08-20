ac5ff23fe2eb562a9af5f2d3da6ae2913df2ddde
Zphyr00 <92588368+Zphyr00@users.noreply.github.com>
2023-08-20 20:26:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_STATIC = 'zphyr-v3';
+const CACHE_STATIC = 'zphyr-v3.1';
 function hndlEventInstall(evt) {
     async function cacheStaticFiles() {
         const files = [
