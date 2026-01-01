ba90b06a65d07655cefaac38a4bd0def403c331e
Zphyr00 <92588368+Zphyr00@users.noreply.github.com>
2026-01-01 20:00:13 +0700

    Update static cache version to zphyr-v9.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_STATIC = 'zphyr-v9.1';
+const CACHE_STATIC = 'zphyr-v9.2';
 function hndlEventInstall(evt) {
     async function cacheStaticFiles() {
         const files = [
